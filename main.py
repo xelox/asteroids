6e309294078fa2868bcd9fff300e608df7f7d57d
xelox <alexdoppiaj@gmail.com>
2024-08-27 18:38:23 +0300

    clock

--- main.py
+++ main.py
@@ -11,13 +11,19 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
+        # handle events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        # render
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        print(dt)
 
 if __name__ == "__main__":
     main()
