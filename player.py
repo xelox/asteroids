74ae7d9837c3e485bcac8fa1299ee8dd9f1f3ed2
xelox <alexdoppiaj@gmail.com>
2024-08-27 19:05:53 +0300

    the cute triangle on the black screen now moves!

--- player.py
+++ player.py
@@ -23,6 +23,10 @@ class Player(circleshape.CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt;
 
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -30,3 +34,7 @@ class Player(circleshape.CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
